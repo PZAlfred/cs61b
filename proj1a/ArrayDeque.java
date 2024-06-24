96d8a9224e3e18d5c60b0d2fe1576d714ed6bb20
PengAir <zhangpengalfred@gmail.com>
2024-06-24 10:14:43 -0600

    array deque version 2

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -88,4 +88,57 @@ public class ArrayDeque<T> {
         }
     }
 
+    /**
+     * Removes and returns the item at the front of the deque. If no such item
+     * exists, returns null.
+     */
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        size -= 1;
+        T value = items[nextFirst];
+        items[nextFirst] = null;
+        if (nextFirst == items.length - 1) {
+            nextFirst = 0;
+        } else {
+            nextFirst += 1;
+        }
+        return value;
+    }
+
+    /**
+     * Removes and returns the item at the back of the deque. If no such item
+     * exists, returns null.
+     */
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        size -= 1;
+        T value = items[nextLast];
+        items[nextLast] = null;
+        if (nextLast == 0) {
+            nextLast = items.length - 1;
+        } else {
+            nextLast -= 1;
+        }
+        return value;
+    }
+
+    /**
+     * Gets the item at the given index, where 0 is the front, 1 is the next item,
+     * and so forth. If no such item exists, returns null. Must not alter the deque!
+     */
+    public T get(int index) {
+        if (size == 0) {
+            return null;
+        }
+        int finalIndex = index + nextFirst - 1;
+        if (finalIndex > items.length - 1) {
+            finalIndex = finalIndex - items.length;
+        }
+        return items[finalIndex];
+    }
+
 }
