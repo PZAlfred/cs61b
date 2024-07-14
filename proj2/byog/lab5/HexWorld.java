530c8f68cddddfd13819dddc7420c6ebd0969927
PZAlfred <zhangpengalfred@gmail.com>
2024-07-14 16:40:07 -0600

    lab5 finished

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.*;
 import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
 import byog.TileEngine.Tileset;
+import edu.princeton.cs.algs4.StdRandom;
 
 import java.util.Random;
 
@@ -13,7 +14,7 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
-    private static final int WIDTH = 60;
+    private static final int WIDTH = 29;
     private static final int HEIGHT = 30;
 
     public static void main(String[] args) {
@@ -25,7 +26,21 @@ public class HexWorld {
                 world[x][y] = Tileset.NOTHING;
             }
         }
-        addHexagon(30, 25, 1, world, Tileset.SAND);
+        int x_start = 3;
+        int y_start = HEIGHT / 2 - 6 + 2;
+        // addHexagon(x_start, y_start, 3, world, Tileset.TREE);
+        // addHexagon(x_start+5, y_start-3, 3, world, Tileset.WATER);
+        // addHexagon(x_start+10, y_start-6, 3, world, Tileset.SAND);
+        // ter.renderFrame(world);
+        int[] I = new int[] { 3, 4, 5, 4, 3 };
+        for (int i = 0; i < 5; i++) {
+            int x = x_start + i * 5;
+            for (int j = 0; j < I[i]; j++) {
+                int y = y_start - 6 + Math.abs(i-2) * 3 + j * 6;
+                TETile t = randomTile();
+                addHexagon(x, y, 3, world, t);
+            }
+        }
         ter.renderFrame(world);
     }
 
@@ -35,7 +50,7 @@ public class HexWorld {
 
     private static void addLine(int x, int y, int lineLength, TETile[][] world, TETile t) {
         for (int i = 0; i < lineLength; i++) {
-            world[x+i][y] = t;
+            world[x + i][y] = t;
         }
     }
 
@@ -58,4 +73,25 @@ public class HexWorld {
         addDownHalf(x - s + 1, y - s, s, world, t);
     }
 
+    private static TETile randomTile() {
+        int tileNum = StdRandom.uniform(7);
+        switch (tileNum) {
+            case 0:
+                return Tileset.WALL;
+            case 1:
+                return Tileset.FLOWER;
+            case 2:
+                return Tileset.GRASS;
+            case 3:
+                return Tileset.WATER;
+            case 4:
+                return Tileset.SAND;
+            case 5:
+                return Tileset.MOUNTAIN;
+            case 6:
+                return Tileset.TREE;
+            default:
+                return Tileset.NOTHING;
+        }
+    }
 }
