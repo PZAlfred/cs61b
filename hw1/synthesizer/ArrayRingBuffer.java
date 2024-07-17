1fbd204208dbd27fcce53ef5947ca04cc2ca77f2
PZAlfred <zhangpengalfred@gmail.com>
2024-07-17 12:18:45 -0600

    fix issues

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -2,22 +2,14 @@ package synthesizer;
 import java.util.Iterator;
 
 public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T>  {
-    /* Index for the next dequeue or peek. */
     private int first;            // index for the next dequeue or peek
-    /* Index for the next enqueue. */
     private int last;
-    /* Array for storing the buffer data. */
     private T[] rb;
 
     /**
      * Create a new ArrayRingBuffer with the given capacity.
      */
     public ArrayRingBuffer(int capacity) {
-        // TODO: Create new array with capacity elements.
-        //       first, last, and fillCount should all be set to 0.
-        //       this.capacity should be set appropriately. Note that the local variable
-        //       here shadows the field we inherit from AbstractBoundedQueue, so
-        //       you'll need to use this.capacity to set the capacity.
         rb = (T[]) new Object[capacity];
         first = 0;
         last = 0;
@@ -32,7 +24,6 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T>  {
      */
     @Override
     public void enqueue(T x) {
-        // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
         if (x == null) {
             return;
         }
@@ -54,7 +45,6 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T>  {
      * covered Monday.
      */
     public T dequeue() {
-        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update
         if (isEmpty()) {
             throw new RuntimeException("Ring buffer underflow");
         } else {
@@ -73,14 +63,12 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T>  {
      * Return oldest item, but don't remove it.
      */
     public T peek() {
-        // TODO: Return the first item. None of your instance variables should change.
         if (isEmpty()) {
             throw new RuntimeException("Nothing in the array");
         }
         return rb[first];
     }
 
-    // TODO: When you get to part 5, implement the needed code to support iteration.
     public Iterator<T> iterator() {
         return new ArrayRingBufferIterator();
     }    
@@ -101,7 +89,7 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T>  {
             T returnItem = rb[wizPos + first];
             wizPos += 1;
             if (wizPos + first == capacity) {
-                wizPos = - first;
+                wizPos = -first;
             }
             return returnItem;
         }
